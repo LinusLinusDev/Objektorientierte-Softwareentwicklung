fa09ad50611e6c8e069f8f09d9dbbb51cbe42a73
Yannick Sandermann <69667558+YanniSani@users.noreply.github.com>
2024-05-13 13:52:27 +0200

    Added exercise 3b of HW2 as .md-file.

--- LearningGroup.py
+++ LearningGroup.py
@@ -46,19 +46,3 @@ if __name__ == '__main__':
 
     print(group)
     print(group2)
-
-'''
-What is the outcome of student1 + student2 + student3?
-
-Since no parentheses were used in this example, the operations are performed sequentially. Therefore, the calculation
-could also be written as follows:
-(student1 + student2) + student3
-
-- First, student1 and student2 are added together. This calls the __add()__-method of the Student class, which returns a
-  new object of type LearningGroup that contains student1 and student2.
-  student1 + student2 -> LearningGroup(student1, student2)
-  
-- Then, the generated object of type LearningGroup is added to student3. This calls the __add()__-method of the 
-  LearningGroup class, which returns a new object of type LearningGroup that contains all three students.
-  LearningGroup(student1, student2) + student3 -> LearningGroup(student1, student2, student3)
-'''
